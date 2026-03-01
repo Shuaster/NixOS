c846f44b9bc2557054c70bb4d2fb372bf216c777
Shuaster <joshuaromanick+git@gmail.com>
2026-02-28 21:07:52 -0500

    Added fix for Home Manager paths with multi-host config

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -5,6 +5,11 @@
     inputs.home-manager.nixosModules.home-manager
   ];
 
+  environment.pathsToLink = [
+    "/share/applications"
+    "/share/xdg-desktop-portal"
+  ];
+
   home-manager = {
     useUserPackages = true;
     extraSpecialArgs = {inherit inputs outputs;};
